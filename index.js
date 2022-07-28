2076b440924b928d4c8fb7b64589c0b97d5cdbd5
John B <100374955+jtbrennan@users.noreply.github.com>
2022-07-28 00:20:07 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+
+axios.get('https://www.starwars.com/news')
+    .then((response) => {
+        console.log(response.data);
+        alert(response.data);
+        var fs = require('fs');
+        var fileContent = response.data;
+        fs.writeFile('retrieved.txt', fileContent, (error) => { /* handle error */ });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+// Axious's intercep
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
+
+
+axios.get('/user/12345')
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
